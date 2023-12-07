970c4e4c23cc819c8e450365a64666084833ff7a
JJiWoNing <jjiwoning12@gmail.com>
2023-12-08 02:20:12 +0900

    feat: SecurityConfig 작성

---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.security.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	private final static String[] ALLOWED_URLS = new String[] {"/", "/sign-up", "/sign-in"};
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+			.csrf(AbstractHttpConfigurer::disable)
+			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // h2 console use
+			.authorizeHttpRequests(requests ->
+				requests.requestMatchers(ALLOWED_URLS).permitAll() // main page permit
+					.requestMatchers(PathRequest.toH2Console()).permitAll() // h2 console permit
+					.anyRequest().authenticated() // need authenticated
+			)
+			.sessionManagement(sessionManagement ->
+				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // not use session -> disable
+			.build();
+	}
+}
